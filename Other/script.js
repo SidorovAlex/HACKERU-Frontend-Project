ee1054f783f713bbe5fea21a6778bdd8d592d624
Sidorov Alexander <salexsalex123@gmail.com>
2023-03-08 22:40:09 +0200

    gamle animal game added

--- Other/script.js
+++ Other/script.js
@@ -26,7 +26,8 @@ const showArr=document.querySelector("#showArr");
 
 let arr = [];
 addNumBtn.addEventListener("click",()=>{
-    arr.push(parseInt(numInp.value))
+    if(numInp.value)
+        arr.push(parseInt(numInp.value))
     numInp.value="";
 })
 showArrBtn.addEventListener("click",()=>{
@@ -53,30 +54,79 @@ sortArrBtn.addEventListener("click",()=>{
 //#endregion
 
 //#region gamble games
+
 const zeroBtn = document.querySelector("#zeroLine");
 const choseAnimalBtn = document.querySelector("#choseAnimal");
+const animalConteiner =document.querySelector(".animal-conteiner");
 
-let stepCounter=[];
+let stepCounter=[0,0,0,0];
 const initCounter = ()=>{
-
-    for (const key in runners) {
-        stepCounter.push(0);
-    }
-    
+    stepCounter=[0,0,0,0];
 }
 const randAnimal = () =>{
-    let rand = Math.floor(Math.random(4))
-    return runners[rand];
+    let rand = Math.floor(Math.random()*4);
+    let animals=["dog","horse","duck","chick"]
+    return animals[rand];
 }
-zeroBtn.addEventListener("click",initCounter());
+const showAnimal=(animal,index)=>{
+    const span = document.createElement("span");
+    const br = document.createElement("br");
+    setTimeout(animalMove(animal,index),1000);
+    span.textContent=`Chosen animal is: ${animal.name} steps:${stepCounter[index]}`;
+    animalConteiner.appendChild(span);
+    animalConteiner.appendChild(br);
+
+    zeroBtn.addEventListener("click",()=>{
+        initCounter();
+        if(animalConteiner.contains(span) && animalConteiner.contains(br)){
+            animalConteiner.removeChild(span);
+            animalConteiner.removeChild(br);
+        }
+    });
+}
+const animalMove = (animal,index) =>{
+    stepCounter[index]+=animal.step;
+}
+const animalVoice = (animal) =>{
+    const span = document.createElement("span")
+    const br = document.createElement("br");
+    span.textContent = animal.voice;
+    animalConteiner.appendChild(span);
+    animalConteiner.appendChild(br);
+    zeroBtn.addEventListener("click",()=>{
+        if(animalConteiner.contains(span) && animalConteiner.contains(br)){
+            animalConteiner.removeChild(span);
+            animalConteiner.removeChild(br);
+        }
+        
+    });
+}
+
 
 choseAnimalBtn.addEventListener("click",()=>{
     let randomAn = randAnimal();
-    console.log(randomAn);
+    switch(true){
+        case randomAn==="dog":
+            showAnimal(runners.dog,0);
+            animalVoice(runners.dog)
+            break;
+        case randomAn==="horse":
+            showAnimal(runners.horse,1)
+            animalVoice(runners.horse)
+            break;
+        case randomAn==="duck":
+            showAnimal(runners.duck,2)
+            animalVoice(runners.duck)
+            break;
+        case randomAn==="chick":
+            showAnimal(runners.chick,3)
+            animalVoice(runners.chick)
+            break;
+    }
 })
 
+//#endregion
 
-
-
+//#region Isolate the rich characters
 
 //#endregion
